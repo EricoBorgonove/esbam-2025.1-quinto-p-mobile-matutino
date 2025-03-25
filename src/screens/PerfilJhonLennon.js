98cf0a793298308345077539108babb2e9728c9b
ericoborgonove <borgonove@gmail.com>
2025-03-25 10:13:24 -0400

    FEATURE: imagens

--- src/screens/PerfilJhonLennon.js
+++ src/screens/PerfilJhonLennon.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const PerfilJhonLennon = () =>{
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Jhon Lennon</Text>
             <Image 
-            source={require('../../assets/images/jhon-lennnon.jpeg')}
-            style={styles.imagem}
+                source={require('../../assets/images/jhon-lennnon.jpeg')}
+                style={styles.imagem}
+            />
+            <TouchableOpacity
+                onPress={()=> Alert.alert('Imagem Clicada')}
+            >
+            <Image
+                source={require('../../assets/images/jhon.jpeg')}
+                style={styles.imagem} 
+                />
+            </TouchableOpacity>
+            <Image
+                source={{uri: 'https://www.digitalmusicnews.com/wp-content/uploads/2024/10/estate_of_john_lennon_taps_ppl_to_manage_neighboring_rights_collections-768x432.png'}}
+                style={{width: 100, height: 100}}
             />
         </View>
     )
